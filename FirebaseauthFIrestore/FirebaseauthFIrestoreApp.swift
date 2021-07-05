a4f1eac5777e9f273b600d40bb7f6ca6345a40af
BilelHattay <BilelHattay>
2021-07-05 15:15:06 +0200

    ok

---
+++ FirebaseauthFIrestore/FirebaseauthFIrestoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseauthFIrestoreApp.swift
+//  FirebaseauthFIrestore
+//
+//  Created by Bilel Hattay on 05/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseauthFIrestoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
